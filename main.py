f01e3c633dc82962b09f0ad7d91eea28555ae223
HarizHajde <harishajdareviccoding@gmail.com>
2025-03-11 20:14:17 +0100

    Count_Words funktion eingefügt

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 
+filepath = "books/frankenstein.txt"
 
 
 def get_book_text(filepath):
     with open(filepath) as content:
-        return content.read()
+        book_string = content.read()
+    return book_string
+
+
+
+def count_words(booktext):
+    word_count = 0
+    splitted_strings = booktext.split()
+    for string in splitted_strings:
+        word_count += 1
+    print(f"total number of words in text: {word_count}")
 
 
 
 def main():
-    filepath = "books/frankenstein.txt"
     booktext = get_book_text(filepath)
     print(booktext)
+    count_words(booktext)
+
 
 main()
